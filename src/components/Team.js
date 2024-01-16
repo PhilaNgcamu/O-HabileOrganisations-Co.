035eea914c6290d13d7e613000063368c09649fb
PhilaNgcamu <philangcamu17@gmail.com>
2024-01-16 08:40:21 +0200

    Change comapany name

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import Logo from "./O-HabileOrgabizations&Co.jpg";
+import Logo from "./logo.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./Team.css";
@@ -9,7 +9,7 @@ const Team = () => {
     {
       name: "Sibongumenzi Sithebe",
       position: "CEO / Founder",
-      bio: "Sibongumenzi Sithebe is the visionary leader behind O-Habile Organisations & Co. With a passion for sustainable architecture and innovative design, he leads the company's mission to empower tomorrow's landscapes.",
+      bio: "Sibongumenzi Sithebe is the visionary leader behind O-Habile Enterprises & Co. With a passion for sustainable architecture and innovative design, he leads the company's mission to empower tomorrow's landscapes.",
     },
     {
       name: "Philasande Ngcamu",
@@ -19,12 +19,12 @@ const Team = () => {
     {
       name: "History",
       position: "Our Journey",
-      bio: "O-Habile Organisations & Co. was founded by Sibongumenzi Sithebe in 2023. O-Habile is a clan name derived from the Sithebe family. The journey of O-Habile began with a vision to redefine the landscape of design and architecture, blending innovation with sustainability. Since its inception, the company has grown to become a symbol of excellence and a pioneer in creating spaces that inspire and endure.",
+      bio: "O-Habile Enterprises & Co. was founded by Sibongumenzi Sithebe in 2023. O-Habile is a clan name derived from the Sithebe family. The journey of O-Habile began with a vision to redefine the landscape of design and architecture, blending innovation with sustainability. Since its inception, the company has grown to become a symbol of excellence and a pioneer in creating spaces that inspire and endure.",
     },
     {
       name: "About Us",
       position: "",
-      bio: `At O-Habile Organisations & Co, we are more than just designers, planners, and architects; we are storytellers, innovators, and custodians of the future. We are a dynamic and forward-thinking firm committed to pushing the boundaries of creativity, innovation, and sustainability in the realms of planning, design, and architecture. While our journey has just begun, our passion, vision, and commitment to excellence set us apart as emerging leaders in the industry.\n\nWe believe in harnessing the collective expertise, creativity, and insights of our team, clients, and stakeholders to co-create solutions that are innovative, functional, and aesthetically pleasing. By fostering an environment of open communication and shared vision, we ensure that every project is a true reflection of collective excellence. With a focus on continuous learning, innovation, and growth, we are committed to establishing a legacy of excellence, integrity, and innovation. Our journey may have just begun, but our vision, passion, and commitment to excellence set the stage for a future filled with endless possibilities.\n\nO-Habile Organisation & Co. is not just a company; it's a vision, a mission, and a promise to redefine the future of design and architecture. Join us on this exciting journey as we embark on a quest to create spaces that inspire, innovate, and endure, setting new standards of excellence in the industry.`,
+      bio: `At O-Habile Enterprises & Co, we are more than just designers, planners, and architects; we are storytellers, innovators, and custodians of the future. We are a dynamic and forward-thinking firm committed to pushing the boundaries of creativity, innovation, and sustainability in the realms of planning, design, and architecture. While our journey has just begun, our passion, vision, and commitment to excellence set us apart as emerging leaders in the industry.\n\nWe believe in harnessing the collective expertise, creativity, and insights of our team, clients, and stakeholders to co-create solutions that are innovative, functional, and aesthetically pleasing. By fostering an environment of open communication and shared vision, we ensure that every project is a true reflection of collective excellence. With a focus on continuous learning, innovation, and growth, we are committed to establishing a legacy of excellence, integrity, and innovation. Our journey may have just begun, but our vision, passion, and commitment to excellence set the stage for a future filled with endless possibilities.\n\nO-Habile Organisation & Co. is not just a company; it's a vision, a mission, and a promise to redefine the future of design and architecture. Join us on this exciting journey as we embark on a quest to create spaces that inspire, innovate, and endure, setting new standards of excellence in the industry.`,
     },
   ];
 
