4884e6e72619caae6df63a4ca5876ba74ad09cf8
PhilaNgcamu <philangcamu17@gmail.com>
2024-01-27 12:08:29 +0200

    COnstrction

--- src/components/Construction.js
+++ src/components/Construction.js
@@ -59,6 +59,73 @@ const Construction = () => {
             team ensures seamless project execution from concept to completion.
           </p>
         </div>
+        <div className="expertise-section">
+          <h2>Our Expertise</h2>
+          <p>
+            Step into a world of craftsmanship and innovation as we offer a
+            comprehensive range of construction services. From residential
+            havens to commercial landmarks, O-Habile Enterprises & Co stands as
+            your trusted partner in bringing visions to reality. Our seasoned
+            team of architects, engineers, and craftsmen is dedicated to
+            delivering projects of the highest quality.
+          </p>
+        </div>
+        <div className="what-sets-us-apart-section">
+          <h2>What Sets Us Apart</h2>
+          <p>
+            At the heart of our success lies a deep-seated belief in the human
+            side of construction. We don't just build structures; we build
+            relationships. Our client-centric approach ensures that your
+            aspirations are not just met but exceeded. We pride ourselves on
+            clear communication, transparency, and a collaborative spirit that
+            transforms every project into a shared journey.
+          </p>
+        </div>
+        <div className="services-offered-section">
+          <h2>Services We Offer</h2>
+          <p>
+            <strong>Residential Construction:</strong> Immerse yourself in the
+            warmth of a home crafted with precision and care. Our residential
+            construction services cater to every detail, ensuring your living
+            space reflects your unique lifestyle.
+          </p>
+          <p>
+            <strong>Commercial Spaces:</strong> Elevate your business with a
+            space that speaks volumes. Whether it's an office, retail outlet, or
+            any commercial venture, we bring functionality and aesthetics
+            together seamlessly.
+          </p>
+          <p>
+            <strong>Renovations and Remodeling:</strong> Breathe new life into
+            existing spaces with our renovation and remodeling expertise. We
+            believe in the transformative power of thoughtful design, enhancing
+            the functionality and beauty of any structure.
+          </p>
+          <p>
+            <strong>Project Management:</strong> Sit back and relax as our
+            experienced project managers take the reins. From planning to
+            execution, we ensure a smooth and efficient process, keeping you
+            informed every step of the way.
+          </p>
+        </div>
+        <div className="our-commitment-section">
+          <h2>Our Commitment</h2>
+          <p>
+            O-Habile Enterprises & Co is not just a construction company; we are
+            your partners in progress. Our commitment to quality, integrity, and
+            client satisfaction is unwavering. Every project is a testament to
+            our dedication to delivering not just buildings but enduring spaces
+            that resonate with life.
+          </p>
+        </div>
+        <div className="contact-us-section">
+          <h2>Embark on Your Construction Journey</h2>
+          <p>
+            Your dream space awaits – let's build it together. Contact O-Habile
+            Enterprises & Co, where craftsmanship meets compassion, and
+            structures become stories.
+          </p>
+        </div>
         <div className="navigation-links">
           <Link to="/services" className="back-link">
             Back to Services
--- src/components/InteriorDesign.js
+++ src/components/InteriorDesign.js
@@ -46,15 +46,16 @@ const InteriorDesign = () => {
         {renderSocialLinks()}
       </div>
       <div className="interior-design-container">
+        <div className="background-img-interior"></div>
         <div className="interior-design">
-          <h1 className="section-heading-interior">Interior Design</h1>
-          <p className="section-description-interior">
+          <h1 className="section-heading">Interior Design</h1>
+          <p className="section-description">
             Transform your spaces with our expert Interior Design services. Our
             team of skilled designers combines creativity and functionality to
             create stunning interiors tailored to your unique preferences.
           </p>
         </div>
-        <div className="details-section">
+        <div className="expertise-section">
           <h2>Our Approach</h2>
           <p>
             We believe in a collaborative approach to interior design. Our team
@@ -63,7 +64,43 @@ const InteriorDesign = () => {
             personality and meets practical needs.
           </p>
         </div>
-        <div className="details-section"></div>
+        <div className="design-process-section">
+          <h2>Our Design Process</h2>
+          <p>
+            Consultation: We begin by understanding your vision, needs, and
+            aspirations. Our collaborative approach ensures that the final
+            design is a true reflection of your lifestyle or business
+            objectives.
+          </p>
+          <p>
+            Conceptualization: Our experienced architects and designers
+            transform your ideas into conceptual designs. We present you with a
+            range of options, allowing you to choose the one that resonates most
+            with your vision.
+          </p>
+          <p>
+            Refinement: Your feedback is invaluable. We work closely with you to
+            refine the chosen design, ensuring it aligns perfectly with your
+            expectations and requirements.
+          </p>
+          <p>
+            Execution: Our dedicated team brings the design to life with
+            precision and attention to detail. We oversee every aspect of the
+            construction process to guarantee the seamless execution of the
+            approved plan.
+          </p>
+        </div>
+        <div className="why-choose-section">
+          <h2>Why Choose Us?</h2>
+          <p>
+            Innovation and Sustainability: We stay ahead of industry trends,
+            integrating innovative solutions and sustainable practices into
+            every design. At O-Habile Enterprises & Co, we don't just design
+            spaces; we create experiences. Explore the possibilities with us,
+            and let your imagination come to life through our interior design
+            expertise.
+          </p>
+        </div>
         <div className="navigation-links">
           <Link to="/services" className="back-link">
             Back to Services
